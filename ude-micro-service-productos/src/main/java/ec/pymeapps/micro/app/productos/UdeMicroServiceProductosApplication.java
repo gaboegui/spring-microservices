a0b75c8105c9181b5e4a650011bc1907ad9534d4
Gabriel Eguiguren <gabo.egui@gmail.com>
2021-01-29 22:18:45 -0500

    configuracion eureka

--- ude-micro-service-productos/src/main/java/ec/pymeapps/micro/app/productos/UdeMicroServiceProductosApplication.java
+++ ude-micro-service-productos/src/main/java/ec/pymeapps/micro/app/productos/UdeMicroServiceProductosApplication.java
@@ -2,7 +2,17 @@ package ec.pymeapps.micro.app.productos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/**
+ * Para correr en otro puerto como argumento de la VM
+ * 
+ * -Dserver.port=9001
+ * 
+ * @author Editor
+ *
+ */
+@EnableEurekaClient
 @SpringBootApplication
 public class UdeMicroServiceProductosApplication {
 
